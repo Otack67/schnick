120b3f91f055ea626ead0b1e26c9d5e745986209
Otack67 <105849948+Otack67@users.noreply.github.com>
2022-05-27 14:10:58 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "score") {
+        scorey = value
+    } else if (name == "stat") {
+        staty = value
+    } else {
+        you = value
+    }
+})
+let staty = 0
+let scorey = 0
+let you = 0
+radio.setGroup(42)
+let me = 0
+you = 0
+let scorem = 0
+scorey = 0
+let statm = 0
+staty = 0
+basic.forever(function () {
+    if (0 == 0 && false) {
+    	
+    }
+})
