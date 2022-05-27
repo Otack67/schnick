bfb4d951e503c5325dfeaa9d460f42c07ca9cfc1
Otack67 <105849948+Otack67@users.noreply.github.com>
2022-05-27 14:22:05 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+function zeigeBild () {
+    if (me == 1) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else if (me == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "score") {
         scorey = value
@@ -10,15 +37,26 @@ radio.onReceivedValue(function (name, value) {
 let staty = 0
 let scorey = 0
 let you = 0
-radio.setGroup(42)
 let me = 0
+radio.setGroup(42)
+me = 1
 you = 0
 let scorem = 0
 scorey = 0
 let statm = 0
 staty = 0
 basic.forever(function () {
-    if (0 == 0 && false) {
-    	
+    zeigeBild()
+    if (statm == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            me += 1
+            if (me > 3) {
+                me = 1
+            }
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            statm = 1
+            radio.sendValue("stat", statm)
+        }
     }
 })
