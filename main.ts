0787784e69610b2c462292ecc60bc6fd5750aef8
Otack67 <105849948+Otack67@users.noreply.github.com>
2022-05-27 15:23:00 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+function ermittleSieger () {
+    sieger = 0
+    if (me == 1 && you == 3) {
+        sieger = 1
+    }
+    if (me == 2 && you == 1) {
+        sieger = 1
+    }
+    if (me == 3 && you == 2) {
+        sieger = 1
+    }
+}
 function zeigeBild () {
     if (me == 1) {
         basic.showLeds(`
@@ -30,10 +42,11 @@ radio.onReceivedValue(function (name, value) {
         scorey = value
     } else if (name == "stat") {
         staty = value
-    } else {
+    } else if (name == "me") {
         you = value
     }
 })
+let sieger = 0
 let staty = 0
 let scorey = 0
 let you = 0
@@ -45,6 +58,7 @@ let scorem = 0
 scorey = 0
 let statm = 0
 staty = 0
+sieger = 0
 basic.forever(function () {
     zeigeBild()
     if (statm == 0) {
@@ -61,6 +75,8 @@ basic.forever(function () {
         }
     }
     if (statm == 1 && staty == 1) {
-    	
+        ermittleSieger()
+        scorem += sieger
+        radio.sendValue("score", scorem)
     }
 })
